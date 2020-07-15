8751df532d0c729228cb48f6a44bf29eec59a649
artyhacker <artyhacker@outlook.com>
2020-07-15 11:52:26 +0800

    chore: 添加目录类别与区域数据

---
+++ src/assets/contents.js
@@ -0,0 +1,199 @@
+const contents = [
+  {
+    id: "上海市",
+    name: "上海市",
+    children: [
+      {
+        id: "长宁区",
+        name: "长宁区",
+        children: [
+          {
+            id: "华阳路街道",
+            name: "华阳路街道",
+            children: [
+              {
+                id: "华阳路街道文化服务中心",
+                name: "华阳路街道文化服务中心",
+                children: [],
+              },
+              { id: "“海之韵”合唱团", name: "“海之韵”合唱团", children: [] },
+            ],
+          },
+          {
+            id: "新华路街道",
+            name: "新华路街道",
+            children: [
+              {
+                id: "新华路街道民俗文化中心",
+                name: "新华路街道民俗文化中心",
+                children: [],
+              },
+              {
+                id: "云荷苑荷塘月色戏剧队",
+                name: "云荷苑荷塘月色戏剧队",
+                children: [],
+              },
+            ],
+          },
+          {
+            id: "江苏路街道",
+            name: "江苏路街道",
+            children: [
+              {
+                id: "江苏路街道文化服务中心",
+                name: "江苏路街道文化服务中心",
+                children: [],
+              },
+              { id: "上钢合唱团", name: "上钢合唱团", children: [] },
+            ],
+          },
+          {
+            id: "天山路街道",
+            name: "天山路街道",
+            children: [
+              {
+                id: "天山路街道民俗文化中心",
+                name: "天山路街道民俗文化中心",
+                children: [],
+              },
+              {
+                id: "天山路街道说唱表演队",
+                name: "天山路街道说唱表演队",
+                children: [],
+              },
+            ],
+          },
+          {
+            id: "周家桥街道",
+            name: "周家桥街道",
+            children: [
+              {
+                id: "周家桥街道文化服务中心",
+                name: "周家桥街道文化服务中心",
+                children: [],
+              },
+            ],
+          },
+          {
+            id: "虹桥街道",
+            name: "虹桥街道",
+            children: [
+              {
+                id: "虹桥街道民俗文化中心",
+                name: "虹桥街道民俗文化中心",
+                children: [],
+              },
+              { id: "虹桥街道图书馆", name: "虹桥街道图书馆", children: [] },
+            ],
+          },
+          {
+            id: "仙霞新村街道",
+            name: "仙霞新村街道",
+            children: [
+              {
+                id: "仙霞新村街道文化服务中心",
+                name: "仙霞新村街道文化服务中心",
+                children: [],
+              },
+            ],
+          },
+          {
+            id: "程家桥街道",
+            name: "程家桥街道",
+            children: [
+              {
+                id: "程家桥社区文化活动中心",
+                name: "程家桥社区文化活动中心",
+                children: [],
+              },
+            ],
+          },
+          {
+            id: "北新泾街道",
+            name: "北新泾街道",
+            children: [
+              {
+                id: "北新泾社区文化活动中心",
+                name: "北新泾社区文化活动中心",
+                children: [],
+              },
+            ],
+          },
+          {
+            id: "新泾镇",
+            name: "新泾镇",
+            children: [
+              {
+                id: "新泾镇社区文化活动中心",
+                name: "新泾镇社区文化活动中心",
+                children: [],
+              },
+            ],
+          },
+          { id: "长宁区博物馆", name: "长宁区博物馆", children: [] },
+          {
+            id: "长宁区文化艺术中心",
+            name: "长宁区文化艺术中心",
+            children: [],
+          },
+          {
+            id: "长宁区少年儿童图书馆",
+            name: "长宁区少年儿童图书馆",
+            children: [],
+          },
+          {
+            id: "长宁区群众艺术馆馆",
+            name: "长宁区群众艺术馆馆",
+            children: [],
+          },
+          { id: "长宁区非遗传承馆", name: "长宁区非遗传承馆", children: [] },
+          { id: "长宁区影剧院", name: "长宁区影剧院", children: [] },
+        ],
+      },
+      {
+        id: "浦东新区",
+        name: "浦东新区",
+        children: [
+          {
+            id: "高桥镇",
+            name: "高桥镇",
+            children: [
+              {
+                id: "高桥镇戏曲沙龙队",
+                name: "高桥镇戏曲沙龙队",
+                children: [],
+              },
+            ],
+          },
+
+          {
+            id: "金桥镇",
+            name: "金桥镇",
+            children: [
+              { id: "金桥镇合唱队", name: "金桥镇合唱队", children: [] },
+            ],
+          },
+
+          { id: "浦东展览馆", name: "浦东展览馆", children: [] },
+        ],
+      },
+      { id: "黄浦区", name: "黄浦区", children: [] },
+      { id: "徐汇区", name: "徐汇区", children: [] },
+      { id: "长宁区", name: "长宁区", children: [] },
+      { id: "静安区", name: "静安区", children: [] },
+      { id: "普陀区", name: "普陀区", children: [] },
+      { id: "虹口区", name: "虹口区", children: [] },
+      { id: "杨浦区", name: "杨浦区", children: [] },
+      { id: "闵行区", name: "闵行区", children: [] },
+      { id: "宝山区", name: "宝山区", children: [] },
+      { id: "嘉定区", name: "嘉定区", children: [] },
+      { id: "金山区", name: "金山区", children: [] },
+      { id: "松江区", name: "松江区", children: [] },
+      { id: "青浦区", name: "青浦区", children: [] },
+      { id: "奉贤区", name: "奉贤区", children: [] },
+      { id: "崇明区", name: "崇明区", children: [] },
+    ],
+  },
+];
+
+export default contents;
