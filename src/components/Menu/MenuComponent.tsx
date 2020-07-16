785c3e620d7c50b291e49110d877d8c6b8d57a76
artyhacker <artyhacker@outlook.com>
2020-07-16 09:16:33 +0800

    perf: 优化系统样式

--- src/components/Menu/MenuComponent.tsx
+++ src/components/Menu/MenuComponent.tsx
@@ -1,13 +1,25 @@
-import React, { FC, useState, useEffect } from "react";
+import React, { FC, useState, useEffect, CSSProperties } from "react";
 import { Menu } from "antd";
 import { Link } from "react-router-dom";
 
 import menuConfig from "../../configs/menu";
 import { isAdmin } from "../../utils/user";
 
+const menuStyle = {
+  fontSize: "16px",
+  fontFamily: "Microsoft YaHei",
+};
+const menuItemStyle: CSSProperties = {
+  textAlign: "center",
+  height: "50px",
+  lineHeight: "50px",
+};
+
 const MenuComponent: FC = () => {
-  const menuList = isAdmin ? menuConfig : menuConfig.filter(m => m.auth === 0);
-  
+  const menuList = isAdmin
+    ? menuConfig
+    : menuConfig.filter((m) => m.auth === 0);
+
   const [keys, setKeys] = useState<string[]>([window.location.pathname]);
   useEffect(() => setKeys([window.location.pathname]), []);
 
@@ -16,9 +28,13 @@ const MenuComponent: FC = () => {
       selectedKeys={keys}
       mode="vertical"
       onClick={(e) => setKeys([e.key as string])}
+      style={menuStyle}
     >
       {menuList.map((menu) => (
-        <Menu.Item key={menu.path} style={{ textAlign: 'center' }}>
+        <Menu.Item
+          key={menu.path}
+          style={menuItemStyle}
+        >
           <Link to={menu.path}>{menu.label}</Link>
         </Menu.Item>
       ))}
