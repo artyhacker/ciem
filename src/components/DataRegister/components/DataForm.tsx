9f21bea65b820a169ae672b948a676a71db7fb36
artyhacker <artyhacker@outlook.com>
2020-07-21 16:12:32 +0800

    chore(数据注册): 添加Excel文件上传解析

--- src/components/DataRegister/components/DataForm.tsx
+++ src/components/DataRegister/components/DataForm.tsx
@@ -1,18 +1,32 @@
 import React, { FC, useState, useCallback } from "react";
 import styles from "./styles.module.css";
-import { Row, Col, Input, Upload, Button, Divider } from "antd";
+import { Row, Col, Input, Upload, Button, Divider, message } from "antd";
+import XLSX from "xlsx";
 import {
   UploadOutlined,
   SaveOutlined,
   ReloadOutlined,
 } from "@ant-design/icons";
 
-import { DataType, DEFAULT_DATA } from "../../../models/data";
+import { DataType, DEFAULT_DATA, ExcelDataType } from "../../../models/data";
 import DataDictMap from "./DataDictMap";
+import { RcFile } from "antd/es/upload/interface";
+import { validateForm } from '../utils';
 
 interface Props {
   onRegister: (data: DataType) => void;
 }
+
+const EXCEL_TYPE_LIST = ["xls", "xlsx", "csv"];
+const fileNameStyle = {
+  flex: 1,
+  height: "2rem",
+  lineHeight: "2rem",
+  marginLeft: ".5rem",
+  border: "1px dashed rgba(0,0,0,0.1)",
+  overflow: "hidden",
+  padding: "0 .5rem",
+};
 
 export type DataMapType = { [key: string]: { id: string; name: string } };
 
@@ -21,20 +35,47 @@ const DataForm: FC<Props> = ({ onRegister }) => {
   const [item, setItem] = useState<DataType>(DEFAULT_DATA);
 
   const onSave = useCallback(() => {
-    const postDataMap = Object.keys(dataMap).map((key) => ({
+    let postDataMap = Object.keys(dataMap).map((key) => ({
       dictId: key,
       ...dataMap[key],
     }));
-    onRegister({
+    postDataMap = postDataMap.filter(pf => (!!pf.id || !!pf.name));
+    const postData = {
       ...item,
       dataMap: postDataMap,
-    });
+    };
+    if (validateForm(postData)) {
+      onRegister(postData);
+    }
   }, [item, dataMap, onRegister]);
 
   const onReset = useCallback(() => {
     setItem(DEFAULT_DATA);
     setDataMap({});
   }, []);
+
+  const beforeUpload = (file: RcFile) => {
+    const index = file.name.lastIndexOf(".");
+    const fileType = file.name.slice(index + 1);
+    const fileName = file.name.slice(0, index);
+    if (EXCEL_TYPE_LIST.includes(fileType)) {
+      const fr = new FileReader();
+      fr.readAsBinaryString(file);
+      fr.onload = () => {
+        const readResult = XLSX.read(fr.result, { type: "binary" });
+        const xData = readResult.Sheets[readResult.SheetNames[0]];
+        const data: ExcelDataType[] = XLSX.utils.sheet_to_json(xData);
+        setItem((prev) => ({ ...prev, tableName: fileName, data }));
+      };
+      fr.onerror = () => {
+        message.error("文件读取失败");
+      };
+      return false;
+    } else {
+      message.error("文件格式错误");
+    }
+    return false;
+  };
 
   return (
     <div className={styles.right}>
@@ -60,11 +101,14 @@ const DataForm: FC<Props> = ({ onRegister }) => {
           />
         </Col>
         <Col span={8}>
-          <Upload>
-            <Button type="primary">
-              <UploadOutlined /> 上传Excel
-            </Button>
-          </Upload>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <Upload action="#" beforeUpload={beforeUpload} fileList={[]}>
+              <Button type="primary">
+                <UploadOutlined /> 上传Excel
+              </Button>
+            </Upload>
+            <div style={fileNameStyle}>{item.tableName || "未上传文件"}</div>
+          </div>
         </Col>
         <Col span={8}>
           <Input
@@ -87,7 +131,7 @@ const DataForm: FC<Props> = ({ onRegister }) => {
           />
         </Col>
         <Col span={8}>
-          <span>{`协议类型：${item.protocol || "HTTP"}；请求方式：${
+          <span style={{ height: '2rem', lineHeight: '2rem' }}>{`协议类型：${item.protocol || "HTTP"}；请求方式：${
             item.method || "POST"
           }`}</span>
         </Col>
