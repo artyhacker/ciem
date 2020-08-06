91e8ac81d54df17dde0e0da1dda4d3a11a921046
artyhacker <artyhacker@outlook.com>
2020-08-06 15:06:41 +0800

    feat(header): 展示当前登录机构名称

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,26 @@
-import React, { FC, useState, useEffect } from 'react';
-import moment from 'moment';
-import styles from './Header.module.css';
-import { SYSTEM_TITLE } from '../../models/global';
-import { Popconfirm, message } from 'antd';
-import axiosInstance, { isOk } from '../../utils/axios';
-import api from '../../configs/api';
-import { clearToken } from '../../utils/tokenUtils';
-import { History } from 'history';
-import { clearPreLocation } from '../../utils/preLocationUtils';
-import getUserInfo from '../../utils/getUserInfo';
+import React, { FC, useState, useEffect } from "react";
+import { UserOutlined } from "@ant-design/icons";
+import moment from "moment";
+import styles from "./Header.module.css";
+import { SYSTEM_TITLE } from "../../models/global";
+import { Popconfirm, message } from "antd";
+import axiosInstance, { isOk } from "../../utils/axios";
+import api from "../../configs/api";
+import { clearToken } from "../../utils/tokenUtils";
+import { History } from "history";
+import { clearPreLocation } from "../../utils/preLocationUtils";
+import getUserInfo from "../../utils/getUserInfo";
 
 interface Props {
   history: History;
 }
 
 const Header: FC<Props> = ({ history }) => {
-  const [time, setTime] = useState(moment().format('YYYY.MM.DD HH:mm'));
+  const [time, setTime] = useState(moment().format("YYYY.MM.DD HH:mm"));
 
   useEffect(() => {
     const i = setInterval(() => {
-      setTime(moment().format('YYYY.MM.DD HH:mm'));
+      setTime(moment().format("YYYY.MM.DD HH:mm"));
     }, 2000);
     return () => clearInterval(i);
   }, []);
@@ -27,25 +28,35 @@ const Header: FC<Props> = ({ history }) => {
   const fetchLogout = async () => {
     const res = await axiosInstance.get(`${api.login}/${getUserInfo().id}`);
     if (isOk(res)) {
-      history.push('/login');
+      history.push("/login");
       clearToken();
       clearPreLocation();
-      message.success('您已安全退出！');
+      message.success("您已安全退出！");
       clearToken();
     } else {
-      message.error('操作失败');
+      message.error("操作失败");
     }
   };
 
+  const { name } = getUserInfo();
+
   return (
     <div className={styles.container}>
-      <div className={styles.icon}/>
+      <div className={styles.icon} />
       <div className={styles.title}>{SYSTEM_TITLE}</div>
       <div className={styles.time}>
-        <div className={styles['logo-time']}/>
-        <span style={{ fontSize: '16px', margin: '0 1rem', flex: '1' }}>{time}</span>
-        <Popconfirm title="确认退出？" onConfirm={fetchLogout} placement="leftBottom">
-          <div className={styles['logo-logout']} title="退出登录"/>
+        <span className={styles["logo-time"]} />
+        <div style={{ fontSize: "16px", margin: "0 1rem" }}>{time}</div>
+        <div style={{ fontSize: "16px", marginRight: "2rem" }}>
+          <UserOutlined style={{ fontSize: "1.3rem", marginRight: ".5rem" }} />
+          {name}
+        </div>
+        <Popconfirm
+          title="确认退出？"
+          onConfirm={fetchLogout}
+          placement="leftBottom"
+        >
+          <div className={styles["logo-logout"]} title="退出登录" />
         </Popconfirm>
       </div>
     </div>
