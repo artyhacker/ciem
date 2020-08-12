6c9decd2c18077456630c48ea6cbeda1e12a4848
artyhacker <artyhacker@outlook.com>
2020-08-12 14:32:27 +0800

    fix: 申请者页面登陆后跳转至“我的申请”

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -1,16 +1,17 @@
 import React, { FC, Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import menuConfig from "../../configs/menu";
+import menuConfig, { MenuItemType } from "../../configs/menu";
 
 const AppRouter: FC = () => {
+  const firstPageUrl = process.env.REACT_APP_TYPE === 'applicant' ? '/my-apply' : 'my-data';
+
   return (
     <Suspense fallback={<div>页面发生错误</div>}>
       <Switch>
-        {/* <Redirect exact from="/" to="/my-data" /> */}
-        {menuConfig.map((menu) => (
+        {menuConfig.map((menu: MenuItemType) => (
           <Route key={menu.path} path={menu.path} component={menu.component} />
         ))}
-        <Redirect exact from="/" to="/my-data" />
+        <Redirect exact from="/" to={firstPageUrl} />
       </Switch>
     </Suspense>
   );
